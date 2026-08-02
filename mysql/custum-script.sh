76af868c6a113d2b43a22959c147ae08dd581f1a
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-08-02 18:10:12 +0530

    modified_code

---
+++ mysql/custum-script.sh
@@ -0,0 +1,15 @@
+
+#sess 71
+
+here code anehdi will get it from siva repo
+
+there /tmp/mysql-root-password.txt   --- file lo pwd anedhi store avuthundi, bcz first init container run ayyi then 
+/tmp/mysql-root-password.txt  ikkada pwd will be stored due to init container
+
+
+
+exec /entrypoint.sh mysqld  --> ultimatega idhe run avthundi first, but konchem understand purpose ki manna 
+if  ...........fi code rasam 
+
+
+Dockerfile anedi main contaniner so idhi run before above steps done 
